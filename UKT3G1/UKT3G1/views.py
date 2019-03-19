8374db6ba45d0315363dd1b30eacbab5ff35fc6f
Tan4eto <canletyougo@gmail.com>
2019-03-19 22:10:04 +0200

    Added login,register,db, Models, Forms files. Updated __init__, views, requirements, runserver.

--- UKT3G1/UKT3G1/views.py
+++ UKT3G1/UKT3G1/views.py
@@ -3,35 +3,67 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, request
+from UKT3G1 import app, db, bcrypt
+from UKT3G1.Forms import RegistrationForm, LoginForm
+from UKT3G1.Models import User, UserTests
+from flask_login import login_user, current_user, logout_user, login_required
 from UKT3G1 import app
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+    return render_template('index.html', title='Home Page', year=datetime.now().year,)
+
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+    return render_template('contact.html', title='Contact', year=datetime.now().year,
+                           message='Your contact page.')
+
 
 @app.route('/about')
 def about():
     """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+    return render_template('about.html', title='About', year=datetime.now().year,
+                           message='Your application description page.')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home.html'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home.html'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home.html'))
