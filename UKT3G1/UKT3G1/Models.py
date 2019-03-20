85c6b5af1edd35663775f76acce1a4be1bbdbb8e
PeteKay <peter.koev@ieee.org>
2019-03-20 09:39:50 +0200

    Fixed register and login forms.

--- UKT3G1/UKT3G1/Models.py
+++ UKT3G1/UKT3G1/Models.py
@@ -8,7 +8,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,15 +18,16 @@ class User(db.Model):
     user_tests = db.relationship('UserTests', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return "User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class UserTests(db.Model):
+    __tablename__ = 'UserTests'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return "Post('{self.title}', '{self.date_posted}')"
