37e334e36a1d3bdefaeb29e7910f48a6a2433fa8
Dar Donkov <darindarinov@gmail.com>
2019-03-23 18:49:30 +0200

    Created a semi-working user login status

--- UKT3G1/UKT3G1/views.py
+++ UKT3G1/UKT3G1/views.py
@@ -15,20 +15,20 @@ from UKT3G1 import app
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template('index.html', title='Home Page', year=datetime.now().year,)
+    return render_template('index.html', title='Home Page', year=datetime.now().year, user=current_user)
 
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template('contact.html', title='Contact', year=datetime.now().year,
-                           message='Your contact page.')
+    return render_template('contact.html', title='Contact', year=datetime.now().year, user=current_user,
+                           message='Your contact page.', )
 
 
 @app.route('/about')
 def about():
     """Renders the about page."""
-    return render_template('about.html', title='About', year=datetime.now().year,
+    return render_template('about.html', title='About', year=datetime.now().year, user=current_user,
                            message='Your application description page.')
 
 
@@ -44,7 +44,7 @@ def register():
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form, user=current_user,)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -60,7 +60,7 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title='Login', form=form, user=current_user,)
 
 
 @app.route("/logout")
