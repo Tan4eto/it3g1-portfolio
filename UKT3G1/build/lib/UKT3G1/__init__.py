7a6d304850f428ec9ac601035a896d4004d07284
PeteKay <peter.koev@ieee.org>
2019-05-11 12:56:09 +0300

    Added a setup file for local path installation of package

---
+++ UKT3G1/build/lib/UKT3G1/__init__.py
@@ -0,0 +1,42 @@
+"""
+The flask application package.
+"""
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+# from UKT3G1.models import User
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id) (session.query(User).get(user_id))
+
+
+
+
+import UKT3G1.views
+
---
+++ UKT3G1/build/lib/UKT3G1/models.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from flask_login import UserMixin
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine, types
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
+from UKT3G1 import app, login_manager
+
+
+
+engine = create_engine('sqlite:///test2.db', echo=True)
+Base = declarative_base()
+
+
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(20), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    image_file = Column(String(20), nullable=True, default='default.jpg')
+    password = Column(String(60), nullable=False)
+    user_tests = relationship('UserTests', backref='user', lazy=True)
+
+    # def set_password(self, password):
+    #     self.password_hash = generate_password_hash(password)
+    #
+    # def check_password(self, password):
+    #     return check_password_hash(self.password_hash, password)
+
+    def __init__(self, username=None, email=None, password=None, id=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.authenticated = True
+        self.id = id
+
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def __repr__(self):
+        return "User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return session.query(User).get(id)
+
+
+class UserTests(Base):
+    __tablename__ = 'usertests'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    date_posted = Column(types.DateTime, nullable=False, default=datetime.utcnow)
+    content = Column(types.Text, nullable=False)
+    post_type = Column(types.Text, nullable=False)
+    user_id = Column(types.Integer, ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return "UserTests('{self.title}', '{self.date_posted}','{self.content}', '{self.user_id}', '{self.post_type}')"
+
+
+# create tables
+Base.metadata.create_all(bind=engine)
+
+# create session
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
